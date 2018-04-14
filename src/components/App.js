a060ecfb7bc4d45ee901084f362375f0ba32eef4
Andrei Todorut <andrei.todorut@target-e.com>
2018-04-14 12:28:15 +0300

    [TASK] adding routes for about, teachers and courses, add dynamic title for about page using the render prop of Route component

--- src/components/App.js
+++ src/components/App.js
@@ -4,10 +4,22 @@ import {
 	Route
 } from 'react-router-dom';
 
-const App = () => (
-  <div className="container">
+import Header from './Header';
+import Home from './Home';
+import About from './About';
+import Teachers from './Teachers';
+import Courses from './Courses';
 
-  </div>
+const App = () => (
+  <BrowserRouter>
+	  <div className="container">
+  		<Header />
+	  	<Route exact path="/" component={Home} />
+	  	<Route path="/about" render={() => <About title="About from app" />} />
+	  	<Route path="/teachers" component={Teachers} />
+	  	<Route path="/courses" componens={Courses} />
+	  </div>
+  </BrowserRouter>
 );
 
 export default App;

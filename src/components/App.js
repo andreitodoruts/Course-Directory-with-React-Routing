426f0b1b4b3d863bf599d9ea63a28608a5722842
Andrei Todorut <andrei.todorut@target-e.com>
2018-04-14 12:52:35 +0300

    [TASK] deep in url

--- src/components/App.js
+++ src/components/App.js
@@ -17,7 +17,7 @@ const App = () => (
 	  	<Route exact path="/" component={Home} />
 	  	<Route path="/about" render={() => <About title="About from app" />} />
 	  	<Route path="/teachers" component={Teachers} />
-	  	<Route path="/courses" componens={Courses} />
+	  	<Route path="/courses" component={Courses} />
 	  </div>
   </BrowserRouter>
 );
